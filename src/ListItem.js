90ac54a28a15f3ca1b2bc633dbef72a8ce345b1c
GarvG <67434601+GarvG@users.noreply.github.com>
2023-03-28 15:42:32 +0530

    all completed

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,19 +1,34 @@
-import React from 'react'
-import './ListItems.css'
-const ListItem = (props) => {
-  const items=props.items;
-  const listItems=items.map(e=>{
-    return<div className='list' key='item.key'>
-        <p>
-            {e.text}
-        </p>
-    </div>
-  })
+import React from "react";
+import "./ListItems.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function ListItems(props) {
+  const items = props.items;
+  const listItems = items.map((item) => {
     return (
-    <div>
-      {listItems}
-    </div>
-  )
+      <div className="list" key={item.key}>
+        <p>
+          <input type="text" 
+          id={item.key} 
+          value={item.text} 
+          onChange ={
+            (e) => {
+              props.setUpdate(e.target.value,item.key)
+            }
+          }
+          />
+          <span>
+            <FontAwesomeIcon
+              className="faicons"
+              icon="trash"
+              onClick={() => props.deleteItem(item.key)}
+            />
+          </span>
+        </p>
+      </div>
+    );
+  });
+  return <div>{listItems}</div>;
 }
 
-export default ListItem
+export default ListItems;
